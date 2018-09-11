ef1750e795a9a52e3105080c883dd35484737059
Daniel Gordi <gordi@cafebazaar.ir>
2018-09-11 19:27:32 +0430

    CPAN script modification

--- AnsiAsterProxLXC/files/cpan.sh
+++ AnsiAsterProxLXC/files/cpan.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 export PERL_MM_USE_DEFAULT=1
 
+if [[ -d "/var/www/html/trunk2" ]]
+then
+cd /var/www/html/trunk2
+cp perl_config/Config.pm `find /usr/ -name Config.pm|grep -v CPANPLUS|grep CPAN` -f
+install CPAN::Meta::Requirements CPAN"|cpan
+echo "$CPAN_TAGS Compress::Zlib Archive::Zip"|cpan 
+echo "$CPAN_TAGS YAML"|cpan
+echo "$CPAN_TAGS YAML::Tiny"|cpan
+echo "$CPAN_TAGS inc::Module::Install"|cpan
+echo "$CPAN_TAGS Net::Server::PreFork "|cpan
+echo "$CPAN_TAGS Text::Template JSON  Asterisk::FastAGI CGI::Session Config::File"|cpan
+echo "$CPAN_TAGS POE::Component::Client::Asterisk::Manager Net::Jabber CGI::Upload Text::CSV "|cpan
+else
+echo "Directory not found"
+exit 1
+fi
