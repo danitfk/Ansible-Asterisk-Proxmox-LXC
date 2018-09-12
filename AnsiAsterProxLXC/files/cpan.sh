5868e4d58763520086d9144bcee136cf68b1e147
danitfk <danial@gordi.ir>
2018-09-12 21:06:02 +0430

    Force replace mirror in CPAN

--- AnsiAsterProxLXC/files/cpan.sh
+++ AnsiAsterProxLXC/files/cpan.sh
@@ -7,7 +7,7 @@ cd /var/www/html/trunk2
 /bin/cp perl_config/Config.pm `find /usr/ -name Config.pm|grep -v CPANPLUS|grep CPAN` -f
 echo "o conf prerequisites_policy follow
 o conf build_requires_install_policy yes"|cpan
-echo "o conf urllist push ftp://mirror.cc.columbia.edu/pub/software/cpan/
+echo "o conf urllist ftp://mirror.cc.columbia.edu/pub/software/cpan/
 o conf commit" | cpan
 echo "install CPAN::Meta::Requirements CPAN"|cpan
 echo "$CPAN_TAGS Compress::Zlib Archive::Zip"|cpan 
