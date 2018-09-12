7228625869c5957ded19e98d5b3779dabd39574b
Daniel Gordi <gordi@cafebazaar.ir>
2018-09-12 14:20:19 +0430

    MySQL Bash Script

---
+++ AnsiAsterProxLXC/files/mysql.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /var/www/html/trunk2/
+echo "create database asterisk;grant all on asterisk.* to 'trunk2'@'localhost' identified by 'mtest';flush privileges;"|mysql
+cat db.sql.gz|gunzip|mysql asterisk
+./clear_all.sh
+./create_config.sh
+echo "meral:D.Rx3mcZSdJpQ
+supremetel:3BX6pTODcUt/k" >> /var/www/html/trunk2/.wwwpasswd
+
