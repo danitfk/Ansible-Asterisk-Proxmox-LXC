ff0b95373f3447ef8b30eaa49534f125987dc86a
danitfk <danial@gordi.ir>
2018-09-19 21:50:37 +0430

    Modify cpan modules

--- AnsiAsterProxLXC/files/cpan.sh
+++ AnsiAsterProxLXC/files/cpan.sh
@@ -5,20 +5,9 @@ if [[ -d "/var/www/html/trunk2" ]]
 then
 cd /var/www/html/trunk2
 /bin/cp perl_config/Config.pm `find /usr/ -name Config.pm|grep -v CPANPLUS|grep CPAN` -f
-echo "o conf prerequisites_policy follow
-o conf build_requires_install_policy yes"|cpan
-echo "o conf urllist ftp://mirror.cc.columbia.edu/pub/software/cpan/
-o conf commit" | cpan
-echo "install CPAN::Meta::Requirements CPAN"|cpan
-echo "$CPAN_TAGS Compress::Zlib Archive::Zip"|cpan 
-echo "$CPAN_TAGS YAML"|cpan
-echo "$CPAN_TAGS YAML::Tiny"|cpan
-echo "$CPAN_TAGS inc::Module::Install"|cpan
-echo "$CPAN_TAGS Net::Server::PreFork "|cpan
-echo "$CPAN_TAGS Text::Template JSON  Asterisk::FastAGI CGI::Session Config::File"|cpan
-echo "$CPAN_TAGS POE::Component::Client::Asterisk::Manager Net::Jabber CGI::Upload Text::CSV "|cpan
-cpan install Text::Template
-else
+wget -O /root/cpan.tar.gz https://github.com/danitfk/Ansible-Asterisk-Proxmox-LXC/raw/master/AnsiAsterProxLXC/files/cpan.tar.gz
+cd /root/
+tar -xf cpan.tar.gz
 echo "Directory not found"
 exit 1
 
