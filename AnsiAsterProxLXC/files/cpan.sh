4159db5a4db12fdc46ea90f9170327a137d56e76
danitfk <danial@gordi.ir>
2018-09-19 22:16:26 +0430

    Fix cpan scriptg

--- AnsiAsterProxLXC/files/cpan.sh
+++ AnsiAsterProxLXC/files/cpan.sh
@@ -5,9 +5,10 @@ if [[ -d "/var/www/html/trunk2" ]]
 then
 cd /var/www/html/trunk2
 /bin/cp perl_config/Config.pm `find /usr/ -name Config.pm|grep -v CPANPLUS|grep CPAN` -f
-wget -O /root/cpan.tar.gz https://github.com/danitfk/Ansible-Asterisk-Proxmox-LXC/raw/master/AnsiAsterProxLXC/files/cpan.tar.gz
+wget -O /root/cpan.tar.gz "https://github.com/danitfk/Ansible-Asterisk-Proxmox-LXC/raw/master/AnsiAsterProxLXC/files/cpan.tar.gz"
 cd /root/
 tar -xf cpan.tar.gz
+else
 echo "Directory not found"
 exit 1
 
