9ea2a90969edfaa9f3d052c318ab286892aa07d9
danitfk <danial@gordi.ir>
2018-09-19 23:20:09 +0430

    Update cpan script

--- AnsiAsterProxLXC/files/cpan.sh
+++ AnsiAsterProxLXC/files/cpan.sh
@@ -8,6 +8,11 @@ cd /var/www/html/trunk2
 wget -O /root/cpan.tar.gz "https://github.com/danitfk/Ansible-Asterisk-Proxmox-LXC/raw/master/AnsiAsterProxLXC/files/cpan.tar.gz"
 cd /root/
 tar -xf cpan.tar.gz
+CPAN_TAGS="o conf prerequisites_policy follow
+o conf build_requires_install_policy yes
+install "
+echo "$CPAN_TAGS Compress::Zlib Archive::Zip"|cpan && echo "$CPAN_TAGS YAML"|cpan && echo "$CPAN_TAGS YAML::Tiny"|cpan && echo "$CPAN_TAGS inc::Module::Install"|cpan && echo "$CPAN_TAGS Net::Server::PreFork "|cpan && echo "$CPAN_TAGS Text::Template JSON  Asterisk::FastAGI CGI::Session Config::File"|cpan && echo "$CPAN_TAGS POE::Component::Client::Asterisk::Manager Net::Jabber CGI::Upload Text::CSV "|cpan
+
 else
 echo "Directory not found"
 exit 1
