4b2a6fe98d90103a5775881e6fedc1fd6f1d1fb9
danitfk <danial@gordi.ir>
2018-09-26 17:43:23 +0330

    Add Script to project

---
+++ run-playbook.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+if [[ "$1" == "" ]];
+then
+echo "Help:"
+echo "      ./run-playbook.sh 192.168.1.1"
+exit 1
+else
+	if [[ ! -f ./playbook.yml ]];
+	then
+	echo "Playbook file not exists"
+	fi
+echo "Detect current IP"
+sleep 0.4
+OLDIP=`cat playbook.yml | grep -o "prox_host_ip.*" | awk {'print $2'}`
+echo "Current IP in Playbook is $OLDIP"
+sleep 0.1
+echo "Replace IP in Playbook"
+sed -i "s/$OLDIP/$1" playbook.yml
+sleep 0.2
+echo "Add IP in Ansible"
+echo "$IP" >> /etc/ansible/hosts
+sleep 0.2
+echo "Run Playbook"
+ansible-playbook playbook.yml
+fi
